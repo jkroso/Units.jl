a911267126807cb85700f516a1d94f918129d3b3
Jake Rosoman <jkroso@gmail.com>
2016-02-18 09:50:29 +1300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Units (Experiment)
+
+This is an attempt to create a unit library for Julia which enables you to dispatch on the abstract unit type. I've implemented enough of the abstract type `Size` that I feel confident it will work for all types. However, it requires some runtime overhead. Which other implementations don't have.
---
+++ main.jl
@@ -0,0 +1,75 @@
+# map magnitudes to their standard name
+const prefix = Dict(1 => :da,
+                    2 => :h,
+                    3 => :k,
+                    6 => :M,
+                    9 => :G,
+                   12 => :T,
+                   15 => :P,
+                   18 => :E,
+                   21 => :Z,
+                   24 => :Y,
+                   -1 => :d,
+                   -2 => :c,
+                   -3 => :m,
+                   -6 => :μ,
+                   -9 => :n,
+                  -12 => :p,
+                  -15 => :f,
+                  -18 => :a,
+                  -21 => :z,
+                  -24 => :y)
+const exponent = ['¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
+
+abstract Unit <: Real
+abstract Size{dimensions} <: Unit
+
+typealias Length Size{1}
+typealias Area Size{2}
+typealias Volume Size{3}
+
+immutable Meter{d,magnitude} <: Size{d}
+  value::Real
+end
+typealias km Meter{1, 3}
+typealias m  Meter{1, 0}
+typealias cm Meter{1,-2}
+typealias mm Meter{1,-3}
+typealias litre Meter{3,-1}
+
+# support the `2cm` syntax
+Base.(:*){m,d}(n::Real, ::Type{Meter{d,m}}) = Meter{d,m}(n)
+
+# Define math functions
+for sym in (:+, :-)
+  @eval function Base.$sym{d}(a::Meter{d}, b::Meter{d})
+    mag, aval, bval = normalize(a, b)
+    Meter{d,mag}($sym(aval, bval))
+  end
+end
+
+# * and / also effect the dimension count
+for sym in (:*, :/)
+  @eval function Base.$sym{da,db}(a::Meter{da},b::Meter{db})
+    mag, aval, bval = normalize(a, b)
+    dimensions = $(sym == :* ? :+ : :-)(da, db)
+    Meter{dimensions,mag}($sym(aval, bval))
+  end
+end
+
+"""
+Determine a common magnitude and adjust both values to that magnitude
+"""
+normalize{amag,bmag,da,db}(a::Meter{da,amag}, b::Meter{db,bmag}) = begin
+  minmag = min(amag, bmag)
+  maxmag = max(amag, bmag)
+  difference = maxmag - minmag
+  if amag == minmag
+    minmag, a.value, b.value * (10 ^ difference)
+  else
+    minmag, a.value * (10 ^ difference), b.value
+  end
+end
+
+Base.show{d,mag}(io::IO, m::Meter{d,mag}) =
+  print(io, m.value, get(prefix, mag, ""), 'm', d > 1 ? exponent[d] : "")
