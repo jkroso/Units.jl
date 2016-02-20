14d2080c78ab9c309ecaa68e598c6a7c123dbbfd
Jake Rosoman <jkroso@gmail.com>
2016-02-20 22:43:14 +1300

    support Ratios

--- main.jl
+++ main.jl
@@ -8,6 +8,21 @@ promote_magnitude(1km², 2mm) == (1e6mm², 2mm)
 """
 function promote_magnitude end
 
+"""
+Print the value without its unit
+"""
+function show_value end
+
+"""
+Print the shorthand notation for a given unit type
+"""
+function show_unit end
+
+"""
+`basefactor(km) == 1000`
+"""
+function basefactor end
+
 # map magnitudes to their standard name
 const prefix = Dict(1 => :da,
                     2 => :h,
@@ -32,6 +47,14 @@ const prefix = Dict(1 => :da,
 const exponent = ['¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
 
 abstract Unit <: Real
+
+show_value(io::IO, u::Unit) = begin
+  isinteger(u.value) && return print_shortest(io, convert(Int, u.value))
+  isa(u.value, Rational) && return print(io, u.value)
+  print_shortest(io, u.value)
+end
+Base.show(io::IO, t::Unit) = (show_value(io, t); show_unit(io, typeof(t)))
+
 abstract Size{dimensions} <: Unit
 
 typealias Length Size{1}
@@ -42,6 +65,7 @@ immutable Meter{d,magnitude} <: Size{d}
   value::Real
 end
 
+basefactor{d,m}(::Type{Meter{d,m}}) = Rational(10) ^ m
 Base.size{d}(::Meter{d}) = (d,)
 
 typealias km  Meter{1, 3}
@@ -97,7 +121,56 @@ for sym in (:*, :/)
   end
 end
 
-Base.show{d,mag}(io::IO, m::Meter{d,mag}) = begin
-  print_shortest(io, m.value)
+show_unit{d,mag}(io::IO, ::Type{Meter{d,mag}}) =
   print(io, get(prefix, mag, ""), 'm', d > 1 ? exponent[d] : "")
+
+const time_factors = Dict(-1000 => :ms,
+                          1 => :s,
+                          60 => :minute,
+                          3600 => :hr)
+
+immutable Time{factor} <: Unit
+  value::Real
+end
+
+show_unit{f}(io::IO, ::Type{Time{f}}) = print(io, time_factors[f])
+basefactor{f}(::Type{Time{f}}) = f
+
+for (factor,name) in time_factors
+  @eval typealias $name Time{$factor}
+end
+
+# support Base.promote(1s, 2hr) == (1s, 3600s)
+Base.promote_rule{f1,f2}(::Type{Time{f1}},::Type{Time{f2}}) = Time{min(f1,f2)}
+Base.convert{f1,f2}(Out::Type{Time{f2}}, s::Time{f1}) = Out(s.value)
+
+for sym in (:+, :-)
+  @eval Base.$sym{T<:Time}(a::T, b::T) = T($sym(a.value, b.value))
+  @eval Base.$sym{fa,fb}(a::Time{fa}, b::Time{fb}) = $sym(promote(a,b)...)
+end
+
+immutable Ratio{Num,Den} <: Unit
+  value::Real
+end
+
+show_unit{Num,Den}(io, ::Type{Ratio{Num,Den}}) = begin
+  show_unit(io, Num)
+  print(io, '/')
+  show_unit(io, Den)
+end
+
+typealias Speed{s<:Size,t<:Time} Ratio{s,t}
+
+# enable `1m/s` and `m/s` syntax
+Base.(:/){T<:Unit}(a::Unit, b::Type{T}) = Ratio{typeof(a),T}(a.value)
+Base.(:/){Num<:Unit,Den<:Unit}(a::Type{Num}, b::Type{Den}) = Ratio{Num,Den}
+
+Base.promote_rule{NA,DA,NB,DB}(a::Type{Ratio{NA,DA}}, b::Type{Ratio{NB,DB}}) =
+  Ratio{promote_rule(NA,NB), promote_rule(DA,DB)}
+Base.convert{N1,D1,N2,D2}(T::Type{Ratio{N2,D2}}, r::Ratio{N1,D1}) =
+  T(r.value * basefactor(N1)//basefactor(N2) * basefactor(D2)//basefactor(D1))
+
+for sym in (:+, :-)
+  @eval Base.$sym{T<:Ratio}(a::T, b::T) = T($sym(a.value, b.value))
+  @eval Base.$sym(a::Ratio, b::Ratio) = $sym(promote(a,b)...)
 end
