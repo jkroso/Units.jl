077fd39a2b3701d09a1d688f2ad73cfa030ec123
Jake Rosoman <jkroso@gmail.com>
2017-09-02 21:20:43 +1200

    tweak

--- Money.jl
+++ Money.jl
@@ -13,10 +13,10 @@ struct Dollar{nation} <: Money value::Real end
 Base.show(io::IO, d::Dollar) = @printf(io, "%0.2f %s", d.value, abbr(typeof(d)))
 
 abbr(::Type{Dollar{c}}) where c = string(c)
-basefactor(::Type{Dollar{abbr}}) where abbr = rates[abbr]
+basefactor(::Type{Dollar{abbr}}) where abbr = precise(1/rates[abbr])
 Base.promote_rule(::Type{<:Dollar}, ::Type{<:Dollar}) = Dollar{:USD}
-Base.convert(::Type{A}, b::Dollar) where A<:Dollar =
-  A(precise(b.value) * basefactor(A)/basefactor(typeof(b)))
+Base.convert(::Type{A}, b::B) where {A<:Dollar,B<:Dollar} =
+  A(precise(b.value) * basefactor(B)/basefactor(A))
 
 const Wage = Ratio{<:Money,<:Time}
 const USD = Dollar{:USD}
