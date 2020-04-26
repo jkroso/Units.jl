d8a4bf83224ece94c8e708dd00247ca685b95aae
Jake Rosoman <jkroso@gmail.com>
2020-04-26 10:37:31 +1000

    use symbols

--- Money.jl
+++ Money.jl
@@ -3,30 +3,44 @@
 import Dates: unix2datetime, today
 import Printf.@printf
 
+struct Money{nation} <: BaseUnit
+  value::Real
+end
+
+const symbols = let
+  file = joinpath(@__DIR__(), "symbols.json")
+  ispath(file) || download("https://api.exchangerate.host/symbols", file)
+  data = parse(MIME("application/json"), read(file))["symbols"]
+  Dict{Symbol,Symbol}((Symbol(k)=>Symbol(v["symbol"]) for (k,v) ∈ data)...)
+end
+
 const rates = let
   file = joinpath(@__DIR__(), "rates.json")
   fstat = stat(file)
   if !ispath(fstat) || unix2datetime(fstat.mtime) < today()
     ispath(fstat) && rm(file)
-    download("http://data.fixer.io/api/latest?access_key=e4778c003b3cc59118912e5bd266b9ff", file)
+    download("https://api.exchangerate.host/latest?base=USD", file)
   end
   data = parse(MIME("application/json"), read(file))["rates"]
   Dict{Symbol,Rational}((Symbol(k)=>1/rationalize(v) for (k,v) ∈ data)...)
 end
 
-abstract type Money <: BaseUnit end
-struct Dollar{nation} <: Money value::Real end
+Base.show(io::IO, d::Money{code}) where code = begin
+  n = seperate(sprint((io, x)->@printf(io, "%0.2f", x), d.value))
+  if symbols[code] == :$
+    write(io, "$n $code")
+  else
+    write(io, "$(symbols[code])$n")
+  end
+end
 
-# sprint(show, 1NZD) == "1.00 NZD"
-Base.show(io::IO, d::Dollar) =
-  write(io, "$(seperate(sprint((io, x)->@printf(io, "%0.2f", x), d.value))) $(abbr(typeof(d)))")
-
-abbr(::Type{Dollar{c}}) where c = string(c)
-basefactor(::Type{Dollar{abbr}}) where abbr = rates[abbr]
-Base.promote_rule(::Type{<:Dollar}, ::Type{<:Dollar}) = Dollar{:USD}
+abbr(::Type{Money{c}}) where c = string(c)
+basefactor(::Type{Money{abbr}}) where abbr = rates[abbr]
+Base.promote_rule(::Type{<:Money}, ::Type{<:Money}) = Money{:USD}
 
 const Wage = Money/Time
 
-for sym in [:USD :NZD :AUD :JPY :EUR :GBP :CNY]
-  @eval const $sym = Dollar{$(QuoteNode(sym))}
+for sym in [:USD :NZD :AUD :JPY :EUR :GBP]
+  @eval const $sym = Money{$(QuoteNode(sym))}
+  symbols[sym] == :$ || @eval const $(Symbol(symbols[sym])) = $sym
 end
