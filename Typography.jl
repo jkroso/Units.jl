780a1f40529cec4b1087d0f514ce0fb27b571cd4
Jake Rosoman <jkroso@gmail.com>
2024-11-27 15:36:34 +1100

    define pixel

--- Typography.jl
+++ Typography.jl
@@ -1,4 +1,4 @@
-@use "." m Length basefactor @abbreviate Unit
+@use "." Length basefactor @abbreviate Unit
 @use "./Imperial.jl" inch
 
 struct Point <: Length
@@ -9,3 +9,12 @@ end
 const POINT_BASEFACTOR = basefactor(inch)/72
 basefactor(::Type{Point}) = POINT_BASEFACTOR
 Base.show(io::IO, p::pt) = invoke(show, Tuple{IO,Unit}, io, round(p))
+
+struct Pixel <: Length
+  value::Number
+end
+@abbreviate px Pixel
+
+const PIXEL_BASEFACTOR = basefactor(inch)/96
+basefactor(::Type{Pixel}) = PIXEL_BASEFACTOR
+Base.show(io::IO, p::px) = invoke(show, Tuple{IO,Unit}, io, round(p))
